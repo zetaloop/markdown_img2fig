418dd522cff6346ec0b571c6489e957a51fd1c8d
zetaloop <zetaloop@outlook.com>
2024-09-10 14:11:35 +0800

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="markdown-img2fig",
-    version="1.0.0",
+    version="1.0.1",
     author="zetaloop",
     author_email="zetaloop@outlook.com",
     description="Convert markdown imgs into captioned <figure>s",
